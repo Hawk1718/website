f551f95c0ae3becd485757e30f8f013cb9c3f234
Soldier <soldiercrp@gmail.com>
2019-08-28 00:22:56 -0500

    UPDATE: Opacity of vertical lines, easing of wires, scroll to function

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ $(document).ready(() => {
   console.log('Ready!')
 
   if ($('.home__page').length > 0) {
+    require('scripts/utils')
+
     require('scripts/home')
     require('scripts/render-globe')
     // require('scripts/svg-animation')
---
+++ src/assets/scripts/utils.js
@@ -0,0 +1,11 @@
+// Event: Scroll to
+$('.scrollto').on('click', function (e) {
+  e.preventDefault()
+  const offset = 0;
+  const target = this.hash;
+
+  $('html, body').stop().animate({
+    'scrollTop': $(target).offset().top - offset
+  }, 1000, 'swing', function () {
+  })
+})
