c2f196175ed97b445bcb83b80ede0bad912285c4
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-01-23 11:36:37 +0100

    Fix for merge conflicts

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,11 +24,7 @@ export default {
       {
         hid: 'g-site-verification',
         name: 'google-site-verification',
-<<<<<<< HEAD
         content: 'roVol1_Xx2vBVf-pZwF4GR4T8NwXQtkA49TqBB4rUVs'
-=======
-        content: ''
->>>>>>> dev
       },
       { hid: 'author', name: 'author', content: author },
       {
