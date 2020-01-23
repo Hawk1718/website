7b18abd9d63b54b88de805fee77aca9d6e939b89
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-01-23 11:14:20 +0100

    Some optimizations. Added google tags and google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,15 @@
+const title = 'Injective Protocol'
+const description =
+  'Injective is the first front-running resistant, layer-2 exchange protocol that unlocks the full potential of borderless finance by supporting derivatives and futures.'
+const author = 'Injective Protocol <admin@injectiveprotocol.com>'
+
 export default {
   mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title,
     htmlAttrs: {
       lang: 'en-US'
     },
@@ -14,7 +19,65 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: description
+      },
+      {
+        hid: 'g-site-verification',
+        name: 'google-site-verification',
+        content: ''
+      },
+      { hid: 'author', name: 'author', content: author },
+      {
+        hid: 'twitter-card',
+        property: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'twitter-site',
+        property: 'twitter:site',
+        content: '@injectiveprotocol'
+      },
+      {
+        hid: 'twitter-creator',
+        property: 'twitter:creator',
+        content: '@injectiveprotocol'
+      },
+      { hid: 'twitter-title', property: 'twitter:title', content: title },
+      { hid: 'og-title', property: 'og:title', content: title },
+      {
+        hid: 'og-image',
+        property: 'og:image',
+        content: `${process.env.NUXT_ENV_APP_URL}/images/share.png`
+      },
+      {
+        hid: 'og-description',
+        property: 'og:description',
+        content: description
+      },
+      {
+        hid: 'og-url',
+        property: 'og:url',
+        content: process.env.NUXT_ENV_APP_URL
+      },
+      {
+        hid: 'og-type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'twitter-description',
+        property: 'twitter:description',
+        content: description
+      },
+      {
+        hid: 'twitter-url',
+        property: 'twitter:url',
+        content: process.env.NUXT_ENV_APP_URL
+      },
+      {
+        hid: 'twitter-image',
+        property: 'twitter:image',
+        content: `${process.env.NUXT_ENV_APP_URL}/images/banner.png`
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
@@ -45,8 +108,20 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/google-gtag',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/dotenv'
   ],
+
+  'google-gtag': {
+    id: 'UA-156829931-1'
+  },
+
+  sitemap: {
+    hostname: 'https://injectiveprotocol.com',
+    gzip: true
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
