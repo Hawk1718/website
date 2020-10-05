0bd42b4f12cdfa1933e9ce22e610191488d48a4d
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-10-05 20:19:32 +0200

    Reverted team section back.

--- components/partials/company/_team.vue
+++ components/partials/company/_team.vue
@@ -216,203 +216,194 @@
         </div>
       </div>
     </div>
-    <div class="team__main">
-      <div v-scroll-reveal class="container">
-        <h2 class="sec-title team__title">Our team</h2>
-        <div class="team__wrap">
-          <div class="team-item">
-            <h3 class="team-item__name">Eric Chen</h3>
-            <p class="team-item__txt">CEO</p>
-            <div class="team-item__bar">
-              <a
-                href="https://www.linkedin.com/in/ericchenmelt/"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="LinkedIn"
-              >
-                <span class="icn">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.5 17.5H14.1667V11.875C14.1667 10.9917 13.175 10.2583 12.2917 10.2583C11.4083 10.2583 10.8333 10.9917 10.8333 11.875V17.5H7.5V7.5H10.8333V9.16667C11.3833 8.275 12.8 7.7 13.75 7.7C15.8333 7.7 17.5 9.4 17.5 11.4583V17.5ZM5.83333 17.5H2.5V7.5H5.83333V17.5ZM4.16667 2.5C4.60869 2.5 5.03262 2.67559 5.34518 2.98816C5.65774 3.30072 5.83333 3.72464 5.83333 4.16667C5.83333 4.60869 5.65774 5.03262 5.34518 5.34518C5.03262 5.65774 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 5.65774 2.98816 5.34518C2.67559 5.03262 2.5 4.60869 2.5 4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-              <a
-                href="https://twitter.com/ericinjective"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="Twitter"
-              >
-                <span class="icn">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M22.46 6C21.69 6.35 20.86 6.58 20 6.69C20.88 6.16 21.56 5.32 21.88 4.31C21.05 4.81 20.13 5.16 19.16 5.36C18.37 4.5 17.26 4 16 4C13.65 4 11.73 5.92 11.73 8.29C11.73 8.63 11.77 8.96 11.84 9.27C8.28004 9.09 5.11004 7.38 3.00004 4.79C2.63004 5.42 2.42004 6.16 2.42004 6.94C2.42004 8.43 3.17004 9.75 4.33004 10.5C3.62004 10.5 2.96004 10.3 2.38004 10C2.38004 10 2.38004 10 2.38004 10.03C2.38004 12.11 3.86004 13.85 5.82004 14.24C5.46004 14.34 5.08004 14.39 4.69004 14.39C4.42004 14.39 4.15004 14.36 3.89004 14.31C4.43004 16 6.00004 17.26 7.89004 17.29C6.43004 18.45 4.58004 19.13 2.56004 19.13C2.22004 19.13 1.88004 19.11 1.54004 19.07C3.44004 20.29 5.70004 21 8.12004 21C16 21 20.33 14.46 20.33 8.79C20.33 8.6 20.33 8.42 20.32 8.23C21.16 7.63 21.88 6.87 22.46 6Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </div>
+
+    <div class="bg-white">
+      <div
+        v-scroll-reveal
+        class="max-w-screen-xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24"
+      >
+        <div class="space-y-12">
+          <div
+            class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl"
+          >
+            <h2
+              class="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl"
+            >
+              Meet our team
+            </h2>
+            <p class="text-xl leading-7 text-gray-500"></p>
           </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Albert Chon</h3>
-            <p class="team-item__txt">CTO</p>
-            <div class="team-item__bar">
-              <a
-                href="https://www.linkedin.com/in/albertchon/"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="LinkedIn"
-              >
-                <span class="icn">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.5 17.5H14.1667V11.875C14.1667 10.9917 13.175 10.2583 12.2917 10.2583C11.4083 10.2583 10.8333 10.9917 10.8333 11.875V17.5H7.5V7.5H10.8333V9.16667C11.3833 8.275 12.8 7.7 13.75 7.7C15.8333 7.7 17.5 9.4 17.5 11.4583V17.5ZM5.83333 17.5H2.5V7.5H5.83333V17.5ZM4.16667 2.5C4.60869 2.5 5.03262 2.67559 5.34518 2.98816C5.65774 3.30072 5.83333 3.72464 5.83333 4.16667C5.83333 4.60869 5.65774 5.03262 5.34518 5.34518C5.03262 5.65774 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 5.65774 2.98816 5.34518C2.67559 5.03262 2.5 4.60869 2.5 4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Max K.</h3>
-            <p class="team-item__txt">Senior engineer</p>
-          </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Markus W.</h3>
-            <p class="team-item__txt">Core engineer</p>
-            <div class="team-item__bar">
-              <a
-                href="https://www.linkedin.com/in/markuswaas/"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="LinkedIn"
-              >
-                <span class="icn">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.5 17.5H14.1667V11.875C14.1667 10.9917 13.175 10.2583 12.2917 10.2583C11.4083 10.2583 10.8333 10.9917 10.8333 11.875V17.5H7.5V7.5H10.8333V9.16667C11.3833 8.275 12.8 7.7 13.75 7.7C15.8333 7.7 17.5 9.4 17.5 11.4583V17.5ZM5.83333 17.5H2.5V7.5H5.83333V17.5ZM4.16667 2.5C4.60869 2.5 5.03262 2.67559 5.34518 2.98816C5.65774 3.30072 5.83333 3.72464 5.83333 4.16667C5.83333 4.60869 5.65774 5.03262 5.34518 5.34518C5.03262 5.65774 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 5.65774 2.98816 5.34518C2.67559 5.03262 2.5 4.60869 2.5 4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Bojan A.</h3>
-            <p class="team-item__txt">Core engineer</p>
-            <div class="team-item__bar">
-              <a
-                href="https://www.linkedin.com/in/bangjelkoski/"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="LinkedIn"
-              >
-                <span class="icn">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.5 17.5H14.1667V11.875C14.1667 10.9917 13.175 10.2583 12.2917 10.2583C11.4083 10.2583 10.8333 10.9917 10.8333 11.875V17.5H7.5V7.5H10.8333V9.16667C11.3833 8.275 12.8 7.7 13.75 7.7C15.8333 7.7 17.5 9.4 17.5 11.4583V17.5ZM5.83333 17.5H2.5V7.5H5.83333V17.5ZM4.16667 2.5C4.60869 2.5 5.03262 2.67559 5.34518 2.98816C5.65774 3.30072 5.83333 3.72464 5.83333 4.16667C5.83333 4.60869 5.65774 5.03262 5.34518 5.34518C5.03262 5.65774 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 5.65774 2.98816 5.34518C2.67559 5.03262 2.5 4.60869 2.5 4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Alexandros A.</h3>
-            <p class="team-item__txt">Core engineer</p>
-          </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Xinran X.</h3>
-            <p class="team-item__txt">BD</p>
-            <div class="team-item__bar">
-              <a
-                href="https://www.linkedin.com/in/xinran-xu-2545b480/"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="LinkedIn"
-              >
-                <span class="icn">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.5 17.5H14.1667V11.875C14.1667 10.9917 13.175 10.2583 12.2917 10.2583C11.4083 10.2583 10.8333 10.9917 10.8333 11.875V17.5H7.5V7.5H10.8333V9.16667C11.3833 8.275 12.8 7.7 13.75 7.7C15.8333 7.7 17.5 9.4 17.5 11.4583V17.5ZM5.83333 17.5H2.5V7.5H5.83333V17.5ZM4.16667 2.5C4.60869 2.5 5.03262 2.67559 5.34518 2.98816C5.65774 3.30072 5.83333 3.72464 5.83333 4.16667C5.83333 4.60869 5.65774 5.03262 5.34518 5.34518C5.03262 5.65774 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 5.65774 2.98816 5.34518C2.67559 5.03262 2.5 4.60869 2.5 4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="team-item">
-            <h3 class="team-item__name">Boris S.</h3>
-            <p class="team-item__txt">Technical PM</p>
-            <div class="team-item__bar">
-              <a
-                href="https://www.linkedin.com/in/bshevchenko/"
-                target="_blank"
-                rel="noreferrer"
-                class="team-item__link"
-                title="LinkedIn"
-              >
-                <span class="icn">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.5 17.5H14.1667V11.875C14.1667 10.9917 13.175 10.2583 12.2917 10.2583C11.4083 10.2583 10.8333 10.9917 10.8333 11.875V17.5H7.5V7.5H10.8333V9.16667C11.3833 8.275 12.8 7.7 13.75 7.7C15.8333 7.7 17.5 9.4 17.5 11.4583V17.5ZM5.83333 17.5H2.5V7.5H5.83333V17.5ZM4.16667 2.5C4.60869 2.5 5.03262 2.67559 5.34518 2.98816C5.65774 3.30072 5.83333 3.72464 5.83333 4.16667C5.83333 4.60869 5.65774 5.03262 5.34518 5.34518C5.03262 5.65774 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 5.65774 2.98816 5.34518C2.67559 5.03262 2.5 4.60869 2.5 4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5Z"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </div>
-          </div>
+          <ul
+            class="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:grid-cols-4 lg:max-w-5xl"
+          >
+            <li v-for="(member, index) in team" :key="`member-${index}`">
+              <div class="space-y-6">
+                <img
+                  class="mx-auto h-24 w-24 rounded-full xl:w-40 xl:h-40"
+                  :src="member.image"
+                  alt=""
+                />
+                <div class="space-y-2">
+                  <div class="leading-6 space-y-1">
+                    <h4 class="font-bold text-lg">{{ member.name }}</h4>
+                    <p class="text-primary-600 text-sm">
+                      {{ member.position }}
+                    </p>
+                    <p class="text-gray-600 text-xs font-normal">
+                      {{ member.bio }}
+                    </p>
+                  </div>
+                  <ul class="flex justify-center space-x-5">
+                    <li>
+                      <a
+                        v-if="member.social.twitter"
+                        :href="member.social.twitter"
+                        class="text-gray-400 hover:text-gray-500 transition ease-in-out duration-150"
+                      >
+                        <span class="sr-only">Twitter</span>
+                        <svg
+                          class="w-5 h-5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        v-if="member.social.linkedin"
+                        :href="member.social.linkedin"
+                        class="text-gray-400 hover:text-gray-500 transition ease-in-out duration-150"
+                      >
+                        <span class="sr-only">LinkedIn</span>
+                        <svg
+                          class="w-5 h-5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        {
+          name: 'Eric Chen',
+          position: 'CEO',
+          bio:
+            'NYU Stern Finance, NYU CS, NYU Blockchain Lab, Protocol researcher at Innovating Capital',
+          image: '/team/eric.jpg',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/ericchenmelt/',
+            twitter: 'https://twitter.com/ericinjective',
+          },
+        },
+        {
+          name: 'Albert Chon',
+          position: 'CTO',
+          bio:
+            'Stanford CS, Applied Cryptography Group, Consultant at OpenZeppelin, Software Engineer at Amazon',
+          image: '/team/albert.jpg',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/albertchon/',
+            twitter: null,
+          },
+        },
+        {
+          name: 'Max Kupriianov',
+          position: 'Senior Core Engineer',
+          bio:
+            'HSE University CS, Senior Golang Developer, Blockchain Architect',
+          image: '/team/max.jpg',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/mkupriianov/',
+            twitter: null,
+          },
+        },
+        {
+          name: 'Bojan Angjelkoski',
+          position: 'JavaScript Engineer',
+          bio: 'University of Belgrade CS MS, Senior Javascript Engineer',
+          image: '/team/bojan.jpg',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/bangjelkoski/',
+            twitter: 'https://twitter.com/bangjelkoski',
+          },
+        },
+        {
+          name: 'Alexandros Athanasopulos',
+          position: 'Blockchain Engineer',
+          bio: 'MSc in Electrical and Computer Engineering NTUA Athens',
+          image: '/team/xalee.jpg',
+          social: {
+            linkedin:
+              'https://www.linkedin.com/in/alexandros-athanasopoulos-a14488164/',
+            twitter: null,
+          },
+        },
+        {
+          name: 'Markus Waas',
+          position: 'Blockchain Engineer',
+          bio: 'Humboldt University of Berlin CS MS, Senior Solidity Developer',
+          image: '/team/markus.jpg',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/markuswaas',
+            twitter: null,
+          },
+        },
+        {
+          name: 'Xinran X.',
+          position: 'Business Development',
+          bio: '',
+          image: '/team/empty.png',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/xinran-xu-2545b480/',
+            twitter: null,
+          },
+        },
+        {
+          name: 'Boris Shevchenko',
+          position: 'Project Manager',
+          bio: '',
+          image: '/team/empty.png',
+          social: {
+            linkedin: 'https://www.linkedin.com/in/bshevchenko/',
+            twitter: null,
+          },
+        },
+        {
+          name: 'Michael Siedlecki',
+          position: 'Frontend Developer',
+          bio: '',
+          image: '/team/empty.png',
+          social: {
+            linkedin: null,
+            twitter: null,
+          },
+        },
+      ],
+    }
+  },
+}
+</script>
