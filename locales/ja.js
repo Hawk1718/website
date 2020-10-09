197575e1625fcf71335284ebf43f9ed1822bf148
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-10-09 15:57:42 +0200

    Added japanese locale strings

---
+++ locales/ja.js
@@ -0,0 +1,128 @@
+export default {
+  nav: {
+    injective_ecosystem: 'Hệ sinh thái của Injective Protocol',
+    blog: 'Blog',
+    about: 'Thông tin',
+    company: 'Công ty',
+  },
+  common: {
+    our_family: 'Gia đình của chúng tôi',
+  },
+  form: {
+    your_email: 'email@example.com',
+    email_error: 'Vui lòng kiểm tra lại địa chỉ email',
+    thanks_waitlist: 'Cám ơn! Chúng tôi đã nhận được yêu cầu của bạn.',
+  },
+  header: {
+    join_waitlist: 'Tham gia danh sách chờ',
+  },
+  ecosystem: {
+    waitlist_title:
+      'Bắt đầu giao dịch với<span>sự tự do thật sự</span> và <br/> <span>không giới hạn</span> <br/> <span>những khả năng</span>',
+    hero_title:
+      'Injective Protocol, một giao thức Layer-2 Defi hoàn toàn phi tập trung',
+    hero_subtitle:
+      ' Một giao thức DeFi được tham số hóa mạnh mẽ, hiệu suất cao hỗ trợ một loạt các dẫn xuất, bao gồm CFD, hoán đổi vô thời hạn và hơn thế nữa.',
+    learn_how: 'Tìm hiểu thêm về cách hoạt động của Injective Protocol',
+    learn_how_text: 'Tìm hiểu sâu hơn về kiến trúc của Injective Protocol',
+    learn_how_docs: 'Khám phá các tài liệu của chúng tôi',
+    heading: 'Những tính năng cốt lõi',
+    injective_chain: 'Chuỗi Injective',
+    injective_chain_text:
+      'Cơ sở hạ tầng hỗ trợ cho các ứng dụng DeFi thế hệ tiếp theo',
+    injective_chain_text2:
+      'Layer-2 Cosmos Zone lưu trữ giao thức giao dịch hoàn toàn phi tập trung đáng tin cậy đầu tiên trên thế giới. Chúng tôi đã xây dựng sổ lệnh hoàn toàn phi tập trung đầu tiên trên thế giới, điều phối thực hiện giao dịch, trình khớp lệnh và môi trường khớp lệnh.',
+    injective_futures: 'Giao thức tương lai Injective',
+    injective_futures_text:
+      'Mở ra một kỷ nguyên mới cho việc truy cập tài chính không bị kiểm soát',
+    injective_futures_text2:
+      'Một giao thức tương lai ngang hàng hoàn toàn phi tập trung hỗ trợ tính năng hoán đổi vĩnh viễn phi tập trung và các hợp đồng chênh lệch (CFD). Điều này sẽ cho phép bất kỳ ai cũng có thể tạo và giao dịch trên các thị trường phái sinh tùy ý chỉ với một nguồn cấp dữ liệu giá.',
+    injective_exchange: 'Sàn giao dịch Injective',
+    injective_exchange_text:
+      ' Đi tiên phong trong một mô hình trao đổi mới như một tiện ích công cộng',
+    injective_exchange_text2:
+      'Cơ sở hạ tầng sàn giao dịch phi tập trung với mã nguồn mở. Chúng tôi thay đổi mô hình kinh doanh của sàn giao dịch truyền thống bằng cách loại bỏ rào cản kỹ thuật gia nhập để một người có thể vận hành sàn phi tập trung một cách phân quyền và mang lại hiệu quả cao trên Injective Protocol.',
+    injective_governance: 'Quản trị Injective',
+    injective_governance_text: ' Do cộng đồng sở hữu và thúc',
+    injective_governance_text2:
+      'Injective hoàn toàn phi tập trung và được quản lý bởi cộng đồng bằng cách sử dụng Injective Token. Bất kỳ ai cũng có thể tham gia với tư cách là một bên liên quan cho dự án của chúng tôi và có tiếng nói thực sự trong việc đưa ra quyết định trong giao thức của chúng tôi.',
+    questions_title:
+      '<span>Những vấn đề nào sẽ được</span> <br /><span> giải quyết bởi Injective?</span>',
+    question_1:
+      'Injective trao quyền làm chủ cho người dùng bằng cách cho phép giao miễn phí và phi tập trung cho tất cả mọi người.',
+    question_2:
+      'Nhiều DEX ngày nay tuyên bố là phi tập trung nhưng thực sự là tập trung ở hầu hết các khía cạnh ngoài quyền lưu ký.Từ việc hạn chế quyền truy cập vào một số nhân khẩu học người dùng nhất định, duy trì quyền kiểm soát độc quyền đối với cơ sở hạ tầng trao đổi, mọi sổ lệnh giới hạn trung tâm DEX mà chúng tôi đã thấy đều có các thành phần tập trung cho phép các nhà giao dịch tuân theo ý muốn của một cơ quan toàn năng nào đó.Đây là vấn đề cơ bản mà Injective sẽ giải quyết.',
+    question_3:
+      ' Cơ sở lý luận chung chống lại phân quyền hoàn toàn là hiệu quả và trải nghiệm người dùng bị hy sinh. Chúng tôi tại Injective bác bỏ quan điểm này và đã cam kết biến tầm nhìn về một giao thức trao đổi có hiệu suất cao, phân quyền và hoàn toàn phi tập trung thành hiện thực.',
+    question_title_2:
+      '<span>Injective</span> <br /><span>khác biệt như thế nào?</span>',
+    question_4: ' Đi tiên phong trong một nền kinh tế phi tập trung mới.',
+    question_5:
+      'Sự khác biệt cơ bản lớn nhất giữa Injective Protocol và các sàn giao dịch khác là chúng tôi làm cho việc giao dịch trở thành một tiện ích công cộng phi tập trung, mang lại cho người dùng và cộng đồng của chúng tôi những giá trị có được từ giao thức trao đổi của chúng tôi.',
+    question_6:
+      'Công nghệ của chúng tôi mang đến một trật tự tăng tốc bằng cách mở rộng quy mô thực hiện và thanh toán giao dịch trên layer 2 ở trạng thái hoàn toàn phi tập trung. Bản chất hoàn toàn phi tập trung của Injective cho phép các cá nhân tạo và giao dịch các thị trường phái sinh mà ngay cả các sàn giao dịch tập trung cũng không thể hỗ trợ.',
+  },
+  company: {
+    heading:
+      'Giúp chúng tôi xây dựng tương lai của<br /><span><span> Tài chính</span> <br /><span>phi tập trung</span></span>',
+    hero_title:
+      'Tạo lập một mạng lưới tài chính phi tập trung miễn phí cho mọi người',
+    hero_subtitle:
+      ' Nhiệm vụ của chúng tôi là tạo ra một hệ thống tài chính tự do và toàn diện hơn thông qua tính phân quyền.',
+    hero_subtitle2:
+      'Bằng cách cung cấp khả năng không hạn chế và chưa từng có để thể hiện các quan điểm đa dạng trong thị trường tài chính phi tập trung, chúng tôi đang cố gắng trao quyền cho các cá nhân có khả năng phân bổ vốn hiệu quả hơn trong xã hội của chúng tôi.',
+    see_open_roles: ' Xem thêm về các vài trò',
+  },
+  footer: {
+    explore: 'Khám phá',
+    exchange: 'Giao dịch',
+    coming_soon: 'Sắp có',
+    careers: 'Nghề nghiệp',
+    we_are_hiring: 'Chúng tôi đang tuyển dụng',
+    developers: 'Lập trình viên',
+    documentation: 'Tài liệu tham khảo',
+    connect: 'Kết nối',
+    contact_us: 'Liên hệ với chúng tôi',
+    terms_and_conditions: 'Các điều khoản và qui định',
+    privacy_policy: 'Chính sách riêng tư',
+    copyright: `Bản quyền thuộc về ${new Date().getFullYear()} Injective Labs, Inc. Mọi quyền lợi đã được bảo lưu (Nghiêm cầm sao chép).`,
+  },
+  home: {
+    waitlist_title:
+      'Bắt đầu giao dịch với <span>sự tự do thật sự</span> và <br /><span> không giới hạn</span> <br /><span>những khả năng</span>',
+    hero_title: 'Nhanh, An toàn và giao dịch hoàn toàn phi tập trung',
+    hero_subtitle:
+      ' Injective là giao thức trao đổi layer2 phi tập trung đầu tiên mở khóa tất cả tiềm năng của thị trường giao dịch phái sinh phi tập trung và DeFi không biên giới.',
+    limitless_title:
+      'Giao dịch không giới hạn trên <br /> <span>sàn giao dịch</span><br /><span>phi tập trung đầu tiên</span>',
+    fully_decentralized: 'Hoàn toàn phi tập trung',
+    fully_decentralized_text:
+      'Injective Chain là một mạng chuyển tiếp sidechain phi tập trung hoàn toàn, đóng vai trò như một nền tảng phái sinh lớp 2, điều phối viên thực hiện giao dịch (TEC) và sổ lệnh phi tập trung. Sự đồng thuận cốt lõi được dựa trên Tendermint.',
+    layer2speed: 'Tốc độ của Layer2',
+    layer2speed_text:
+      'Injective Chain cung cấp vùng chốt Ethereum hai chiều cho Ether và các token ERC-20 được chuyển sang Injective Chain cũng như môi trường thực thi tương thích EVM cho các ứng dụng DeFi. Vùng chốt dựa trên Peggy và việc thực thi EVM dựa trên Ethermint.',
+    orderbook_model: 'Mô hình sổ lệnh',
+    orderbook_model_text:
+      'Sổ lệnh phi tập trung của Injective là một sổ lệnh hoàn toàn phi tập trung dựa trên nền tảng 0x cho phép chuyển tiếp các lệnh sidechain thông qua việc thanh toán “on-chain” - một hình thức vận hành phi tập trung của việc chuyển tiếp lệnh tập trung“off-chain” truyền thống vốn được sử dụng bởi hầu hết tất cả các sàn giao dịch phi tập trung trong sổ lệnh giới hạn trung tâm.',
+    unlimited_markets: ' Thị trường không giới hạn',
+    unlimited_markets_text:
+      'Việc tạo thị trường để giao dịch là hoàn toàn mở và miễn phí. Do đó bất kỳ ai cũng có thể tạo thị trường chỉ bằng cách sử dụng nguồn cấp dữ liệu giá cả và triển khai chúng một cách hiệu quả trong hệ sinh thái rộng lớn của Injective.',
+    zero_gas_fees: 'Phí gas bằng 0',
+    zero_gas_fees_text:
+      ' Với cấu trúc layer 2 độc đáo, Injective có thể tránh được cả việc tắc nghẽn mạng và các vấn đề về phí gas cao. Cấu trúc phí gas bằng 0 này cho phép bất kỳ ai cũng có thể giao dịch tự do mà không phải lo lắng về phí gas cao hoặc mạng chậm trễ.',
+    trailblazing_title:
+      'Dẫn đầu cho bình minh mới của <br /><span> <span>tài chính</span> <br /><span>phi tập trung</span></span> ',
+    trailblazing_subtitle:
+      ' Giao thức Injective cung cấp cho các nhà giao dịch quyền truy cập không giới hạn vào các thị trường phái sinh phi tập trung mới mà không có bất kỳ sự hạn chế nào.',
+    trailblazing_item_1: 'Giao dịch mọi thứ',
+    trailblazing_item_1_text:
+      ' Injective cho phép bạn tự do tạo và giao dịch trên bất kỳ thị trường phái sinh nào mà bạn chọn.',
+    learn_more: 'Tìm hiểu thêm',
+    trailblazing_item_2: 'Hưởng lợi nhuận',
+    trailblazing_item_2_text:
+      ' Hưởng lợi nhuận cross-chain trên Ethereum, Cosmos và các mạng phi tập trung khác.',
+    trailblazing_item_3: ' Cộng đồng nắm quyền sở hữu',
+    trailblazing_item_3_text:
+      ' Tham gia quản trị và định hướng tương lai của Injective Protocol.',
+  },
+}
