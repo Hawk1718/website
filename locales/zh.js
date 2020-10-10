7f0c13da3fa905cc9adb2379cd48bdcd3b921601
Eric Chen <ericchen.melt@gmail.com>
2020-10-10 16:15:09 -0400

    chinese translation update

--- locales/zh.js
+++ locales/zh.js
@@ -1,110 +1,115 @@
 export default {
+  //
+}
+
+
+export default {
   nav: {
-    injective_ecosystem: 'Injective 生态系统',
+    injective_ecosystem: 'Injective生态',
     blog: '博客',
     about: '关于',
-    company: '公司',
+    company: '关于我们',
   },
   common: {
-    our_family: '我们的合作伙伴',
+    our_family: '合作伙伴',
   },
   form: {
     your_email: 'email@example.com',
-    email_error: '请确认邮箱地址',
-    thanks_waitlist: '谢谢！我们已经收到您的提交',
+    email_error: '请输入邮箱地址',
+    thanks_waitlist: '谢谢！您提交的信息已收到。',
   },
   header: {
-    join_waitlist: '交易平台等待名单',
-
-    // Todo:: translate
-    chat: 'Chat',
-    join_the_future:
-      'Join the Future of <br /><span>Decentralized Finance</span>',
+    join_waitlist: '交易平台等候名单',
   },
   ecosystem: {
-    waitlist_title: '开启交易的<span>无限可能</span>',
-    hero_title: 'Injective Protocol, 一个完全去中心化的 Layer-2 DeFi 协议',
+    waitlist_title:
+      '开启交易的<span>无限可能</span>',
+    hero_title:
+      'Injective Protocol, 一个完全去中心化的Layer-2 DeFi协议',
     hero_subtitle:
-      '一个强大的、可高度参数化的DeFi协议，以支持去中心化的差价合约(CFD)、永续掉期等各类衍生品市场',
-    learn_how: '了解 Injective 如何运作',
-    learn_how_text: '深入了解Injective的架构',
-    learn_how_docs: '查看相关资料',
+      '一个强健、高度可参数化的DeFi衍生品交易协议，以支持去中心化的差价合约(CFD)、永续掉期等各类市场',
+    learn_how: '了解Injective如何运作',
+    learn_how_text:
+      '深入了解Injective的架构',
+    learn_how_docs: '查看更多资料',
     heading: '核心功能',
-    injective_chain: 'Injective 链',
-    injective_chain_text: '支持下一代DeFi应用程序的基础架构',
+    injective_chain: 'Injective链',
+    injective_chain_text:
+      '强力驱动下一代DeFi应用的基础设施',
     injective_chain_text2:
-      'Cosmos上基于 layer-2 的第一个无需验证和完全去中心化的交易协议。我们建立了业内首个完全去中心化的订单簿、交易执行协调器、订单撮合和执行交易的环境。',
-    injective_futures: 'Injective 衍生品协议',
-    injective_futures_text: '解锁无限制金融的新纪元',
+      '首个Layer-2 扩展的Cosmos Zone零信任及完全去中心化的交易协议。我们构建了业内第一个完全去中心化的订单簿、交易执行协调器、订单撮合器和订单执行的综合环境。',
+    injective_futures: 'Injective衍生品协议',
+    injective_futures_text:
+      '解锁无限制金融的新纪元',
     injective_futures_text2:
-      '一个完全去中心化的点对点衍生品协议，支持永续掉期，差价合约  (CFD) 等。仅通过喂价，任何人均可创建衍生品市场。',
-    injective_exchange: 'Injective 交易平台',
-    injective_exchange_text: '让交易成为公共设施的先锋平台',
+      '一个完全去中心化的点对点衍生品协议，支持永续掉期、差价合约（CFD）等。仅通过喂价，任何人均可创建衍生品市场。',
+    injective_exchange: 'Injective交易平台',
+    injective_exchange_text:
+      '让交易成为公共设施的先锋平台',
     injective_exchange_text2:
-      '完全开源的基础平台设施跳过现有中心化交易平台的介入。Injective无技术壁垒，使任何用户均可在平台上进行高性能交易。',
-    injective_governance: 'Injective 治理',
-    injective_governance_text: '社区自治和驱动。',
+      '作为一个完全开源的基础设施平台，我们颠覆了传统的商业模式，使参与者可以跨越技术需求门槛在Injective上进行高性能、安全公正的交易。',
+    injective_governance: 'Injective治理',
+    injective_governance_text: '社区自治及驱动',
     injective_governance_text2:
-      'Injective 是去中心化的，社区通过通证实现治理功能，任何通证持有人均可参与并且对整个协议的治理拥有发言权。',
-    questions_title: '<span>Injective</span>解决了哪些问题',
+      '去中心化的Injective将由社区通过代币治理，任何代币持有人均可参与并且对整个协议的治理拥有真实的发言权。',
+    questions_title:
+      '<span>Injective解决了</span> <br /><span> 什么问题？</span>',
     question_1:
-      'Injective 将权力移交至用户，让用户实现真正自由和去中心化的交易。',
+      'Injective将权力下放至用户，让用户实现真正的自由和去中心化交易。',
     question_2:
-      '当今众多去中心化交易平台在除了托管方面都具备相当中心化的设计。从限制某些地域的用户访问、保持对交易基础结构的专有控制、到在某些情况下要求KYC，我们看到的使用“中央限价订单簿”模型的去中心化交易所都具有中心化的组件，使交易者服从于其权威。这是Injective要解决的根本问题。',
+      '当今的许多DEX倡导他们的去中心化属性，然而我们在除了托管方面都能看到相当中心化的模块。从限制某些地域的用户访问，到保持对交易基础结构的专有控制，我们看到的所有“中央限价订单簿”DEX都具有中心化的组件，使交易者服从于其权威。这是Injective解决的根本问题。',
     question_3:
-      'Injective不认同去中心化即牺牲了效率和用户体验，并致力于将高性能、无限制和完全去中心化的交易协议的愿景变为现实。',
-    question_title_2: '<span>Injective</span> <br /><span>有何不同?</span>',
-    question_4: '开拓全新的去中心化金融.',
+      '普遍对于去中心的负面认知是基于其效率及使用体验的牺牲，我们Injective并不认同这个观点，并已致力于将一个高性能、无限制和完全去中心化的交易协议愿景变为现实。',
+    question_title_2:
+      '<span>Injective</span> <br /><span>有何不同？</span>',
+    question_4: '开拓新的去中心化经济体制。',
     question_5:
-      'Injective Protocol 与其他交易平台的根本区别在于，我们使“交易”成为一种去中心化的“公共设施”，从而使我们的用户和社区从平台中获取价值。',
+      'Injective Protocol与其他交易平台的根本差异在于，我们使“交易”成为一种“公共设施”，从而将平台协议的完整价值反馈给用户和社区。',
     question_6:
-      '我们基于 layer-2 扩展交易的执行和结算技术，为速度带来一个量级的提升。Injective完全去中心化的性质允许用户创建自己选择的任何衍生品市场并进行交易，从而在其他交易所未触及的市场开辟了无限的机会。',
+      '我们的技术能在不牺牲去中心化属性的前提下，在layer-2上扩展交易执行和结算，为速度带来一个量级的提升。Injective的完全去中心化性质允许用户免许可的创建及交易各种连中心化交易所都无法提供的衍生品交易市场。',
   },
   company: {
     heading:
-      '与我们共同打造 <br /><span><span> 去中心化金融</span></span>的未来',
-    hero_title: '为所有用户打造一个完全自由的去中心化金融网络',
-    hero_subtitle: '我们的使命是通过权力下放建立一个更加自由和包容的金融体系。',
+      '与我们共同打造 <br /><span><span> 去中心化的 </span> <br /><span>未来</span></span>',
+    hero_title:
+      '为所有人创建一个真正自由且去中心化的金融网络',
+    hero_subtitle:
+      '我们的使命是通过权力下放建立一个更加自由和包容的金融体系。',
     hero_subtitle2:
-      '通过给予个人在去中心化金融市场中不受限制地表达各自观点的权力，我们致力于使个人获得更有效地在社会中分配资本的能力。',
+      '通过给与个人在去中心化金融市场中不受限制地表达各自观点的权力，我们致力于使个人获得更有效地在社会中分配资本的能力。',
     see_open_roles: '查看招聘信息',
-
-    // Todo::translate
-    our_team: 'Our Team',
   },
   footer: {
-    explore: '查阅更多',
-    exchange: 'Exchange',
-    coming_soon: '即将入驻',
-    careers: '职业机会',
+    explore: '查找',
+    exchange: '交易平台',
+    coming_soon: '即将上线',
+    careers: '人才招聘',
     we_are_hiring: '招聘中',
     developers: '开发者',
-    documentation: '相关文件',
-    connect: '联系',
-    contact_us: '联系我们',
-    terms_and_conditions: 'Terms and Conditions',
-    privacy_policy: '隐私协议',
-    copyright: `Copyright © ${new Date().getFullYear()} Injective Labs, Inc. All rights reserved.`,
-
-    // Todo::translate
-    media_kit: 'Media Kit',
+    documentation: '文档',
+    connect: '联系我们',
+    contact_us: '邮件联系',
+    terms_and_conditions: '条款和条件',
+    privacy_policy: '隐私政策',
+    copyright: `版权属于 © ${new Date().getFullYear()} Injective Labs, Inc. 并保留所有权利`,
   },
   home: {
-    waitlist_title: '开启交易的<span>无限可能</span> ',
-    hero_title: '快速、安全、完全去中心化交易',
+    waitlist_title:
+      '开启交易的 <span>无限可能</span>',
+    hero_title: '快速、安全及完全去中心化交易',
     hero_subtitle:
-      'Injective是业内首个 layer-2 去中心化交易协议，可释放无边界去中心化金融的全部潜力。',
+      'Injective是业内首个layer-2去中心化交易协议，可释放无边界去中心化金融的全部潜力。',
     limitless_title:
-      '在首个<br /> <span>完全去中心化</span><br /><span>交易平台上<br /> <span>进行无限制交易</span>',
+      '在首个完全去中心化<br /> <span>交易平台上</span><br /><span>进行无限制交易</span>',
     fully_decentralized: '完全去中心化',
     fully_decentralized_text:
-      'Injective 链是一个完全去中心化的侧链网络，可用作 layer-2 衍生品平台、交易协调器 (TEC) 和去中心化的订单簿。其核心共识基于Tendermint。',
-    layer2speed: 'Layer-2 高速撮合',
+      'Injective链是一个完全去中心化的侧链网络，可用作layer-2衍生品平台、交易协调器(TEC) 和去中心化的订单簿。其核心共识基于Tendermint。',
+    layer2speed: 'Layer-2高速撮合',
     layer2speed_text:
-      'Injective 链为以太坊和ERC-20通证提供了双向锚定的以太坊peg-zone，以将其转移到Injective 链，并为DeFi应用程序提供与EVM兼容的执行环境。其中peg-zone基于Peggy，而EVM基于以太坊执行。',
+      'Injective链为ETH和其他ERC-20代币提供了双向锚定的以太坊peg-zone，以将其转移到Injective链，并为DeFi应用程序提供与EVM兼容的执行环境。其中peg-zone基于Peggy，而EVM基于以太坊运行。',
     orderbook_model: '订单簿模型',
     orderbook_model_text:
-      'Injective的订单簿是基于0x并实现完全去中心化的，可完成带有链上结算功能的侧链订单交易。而如今几乎所有使用中央限价订单簿的去中心化交易平台皆通过链下结算。',
+      "Injective的订单簿基于0x并实现完全去中心化的，可完成带有链上结算功能的侧链订单交易。而如今几乎所有使用中央限价订单簿的去中心化交易平台皆通过链下结算。",
     unlimited_markets: '无限制的市场',
     unlimited_markets_text:
       '在Injective上创建市场是完全开放且无需许可的，任何人只需通过喂价便可创建交易市场并将其部署到更广阔的Injective生态系统中。',
@@ -112,17 +117,20 @@ export default {
     zero_gas_fees_text:
       '基于独特的layer-2结构，Injective能够避免网络堵塞及其产生的高昂gas费用。0 gas费可以使用户自由交易，而不必担心高额手续费或网络延迟。',
     trailblazing_title:
-      '开拓去中心化金融的<br /><span> <span>新曙光</span></span>',
+      '开拓去中心化金融的 <br /><span> <span>新曙光</span>',
     trailblazing_subtitle:
-      'Injective Protocol 为交易者提供了前所未有的进入去中心化衍生品市场的机会。',
-    trailblazing_item_1: '可交易任何市场',
+      'Injective Protocol为交易者提供了前所未有的进入去中心化衍生品市场的机会。',
+    trailblazing_item_1: '交易无拘束',
     trailblazing_item_1_text:
-      'Injective 允许用户创建自己选择的任何衍生品市场并进行交易。',
+      'Injective允许用户创建自己选择的任何衍生品市场并进行交易。',
     learn_more: '了解更多',
     trailblazing_item_2: '赚取收益',
     trailblazing_item_2_text:
-      '在以太坊、Cosmos和其他去中心化网络上赚取跨链收益。',
+      '在以太坊、 Cosmos和其他去中心化网络上赚取跨链收益。',
     trailblazing_item_3: '社区自治',
-    trailblazing_item_3_text: '社区成员可参与项目的生态发展与社区治理。',
+    trailblazing_item_3_text:
+      '社区成员可参与项目的生态发展与社区治理。',
   },
 }
+
+
