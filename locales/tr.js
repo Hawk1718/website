5cf15598b8a1cf5efb119cfd3445eecf3e006189
Hawk1718 <72894616+Hawk1718@users.noreply.github.com>
2020-10-15 02:00:29 -0400

    Update tr.js

--- locales/tr.js
+++ locales/tr.js
@@ -17,7 +17,8 @@ export default {
   header: {
     chat: 'Sohbet',
     join_waitlist: 'Borsa Bekleme Listesi',
-    join_the_future: "<span>Merkeziyetsiz Finans</span>' ın geleceğine katılın",
+    join_the_future:
+      '<br /><span>Merkeziyetsiz Finans</span>\' ın geleceğine katılın',
   },
   ecosystem: {
     waitlist_title:
@@ -26,7 +27,7 @@ export default {
       'Injective Protocol, tamamen merkeziyetsiz bir Layer-2 DeFi protokolü',
     hero_subtitle:
       'CFD, Perpetual Swap ve daha bir çok çeşitli türev piyasaları destekleyen, sağlam ve yüksek parametrelenebilir bir DeFi protokolü.',
-    learn_how: "Injective'in nasıl çalıştığını öğrenin",
+    learn_how: 'Injective\'in nasıl çalıştığını öğrenin',
     learn_how_text:
       'Injective’in mimarisi hakkında yüksek seviye anlayışa sahip olun',
     learn_how_docs: 'Dokümanlarımızı keşfedin',
@@ -40,15 +41,14 @@ export default {
     injective_futures_text:
       'Finansal erişimde izinsiz yeni bir çağın kapılarını açın',
     injective_futures_text2:
-      "Tamamen merkeziyetsiz olarak Perpetual Swap ve fark sözleşmelerini (CFD'ler) destekleyen, eşler arası bir vadeli işlemler protokolü. Bu sayede herkese, istenilen türev piyasasında sadece bir fiyat akışıyla işlem oluşturma ve ticaret yapabilme olanağı sağlanacaktır.",
+      'Tamamen merkeziyetsiz olarak Perpetual Swap ve fark sözleşmelerini (CFD\'ler) destekleyen, eşler arası bir vadeli işlemler protokolü. Bu sayede herkese, sadece bir fiyat akışıyla istenilen türev piyasasını oluşturma ve bu piyasalarda işlem yapabilme olanağı sağlanacaktır.',
     injective_exchange: 'Injective Borsası',
     injective_exchange_text:
       'Kamu hizmeti anlayışıyla yeni bir borsa modeline öncülük etmek',
     injective_exchange_text2:
-      'Mevcut merkezi borsa modelini aradan çıkartan, tamamen açık kaynak kodlu bir borsa altyapısı.Injective, yüksek performanslı bir borsa üzerinde işlem yapmak için giriş yapmak isteyen herhangi birinin önündeki teknik engelleri ortadan kaldırır.',
+      'Mevcut merkezi borsa modelini aradan çıkartan, tamamen açık kaynak kodlu bir borsa altyapısı. Injective, yüksek performanslı bir borsa üzerinde işlem yapmak için giriş yapmak isteyen herhangi birinin önündeki teknik engelleri ortadan kaldırır.',
     injective_governance: 'Injective Yönetişimi',
-    injective_governance_text:
-      'Topluluk tarafından sahip olunan ve yönetilen bir yapı',
+    injective_governance_text: 'Topluluk tarafından sahip olunan ve yönetilen bir yapı',
     injective_governance_text2:
       'Injective merkeziyetsizdir ve topluluk tarafından Injective Token kullanılarak yönetilir. İsteyen herkes projemizin bir paydaşı olarak aramıza katılabilir ve protokolümüz ile ilgili karar verilirken gerçek bir söz hakkına sahip olabilir.',
     questions_title:
@@ -56,12 +56,12 @@ export default {
     question_1:
       'Injective, herkes için merkeziyetsiz ve gerçekten ücretsiz ticaret olanağı sunarak, insanlara gücünü geri veriyor.',
     question_2:
-      "Bugün bir çok DEX, merkeziyetsiz olduğunu iddia etmesine rağmen gerçekte emanet sistemi hariç olmak üzere bir çok acıdan merkezidir. Belirli kullanıcı demografilerine erişimi kısıtlamaktan, borsa altyapısı üzerinde tescilli kontrolü sürdürmeye kadar, bugüne kadar gördüğümüz merkezi limit emir defterine sahip olan DEX ler dahi merkezileşmiş bileşenler barındırır. Böylece ticaret yapanlar, güçlü merkezi otoritenin iradesine maruz kalırlar. İşte bu Injective'in çözdüğü temel problemdir.",
+      'Bugün bir çok DEX, merkeziyetsiz olduğunu iddia etmesine rağmen gerçekte emanet sistemi hariç olmak üzere bir çok açıdan merkezidir. Belirli kullanıcı demografilerine erişimi kısıtlamaktan, borsa altyapısı üzerinde tescilli kontrolü sürdürmeye kadar, bugüne kadar gördüğümüz merkezi limit emir defterine sahip olan DEXler dahi merkezileşmiş bileşenler barındırır. Böylece ticaret yapanlar, güçlü merkezi otoritenin iradesine maruz kalırlar. İşte bu, Injective\'in çözdüğü temel problemdir.',
     question_3:
-      'Tam merkeziyetsizliğe karşı olan ortak fikir, verimliliğin ve kullanıcı deneyiminin feda edildiğini öne sürer.Injective olarak bu fikri redderek; yüksek performanslı, izinsiz ve tamamen merkeziyetsiz bir borsa protokolü vizyonumuzu gerçeğe dönüştürmek için kendimizi adadık.',
+      'Tam merkeziyetsizliğe karşı olan ortak fikir, verimliliğin ve kullanıcı deneyiminin feda edildiğini öne sürer. Injective olarak bu fikri redderek; yüksek performanslı, izinsiz ve tamamen merkeziyetsiz bir borsa protokolü vizyonumuzu gerçeğe dönüştürmek için kendimizi adadık.',
     question_title_2:
       '<span>Injective</span> <br /><span>Ne Şekilde Farklı?</span>',
-    question_4: 'Yeni bir merkeziyetsiz ekonomiye öncülük etmek',
+    question_4: 'Yeni bir merkeziyetsiz ekonomiye öncülük etmek...',
     question_5:
       'Injective Protocol ile diğer borsalar arasındaki en büyük temel fark, kullanıcılarımız ve topluluğumuzca borsa protokolümüzün sonucunda oluşan değerin elde edilmesini sağlayan merkeziyetsiz bir kamu hizmeti sunmamızdır.',
     question_6:
@@ -69,8 +69,9 @@ export default {
   },
   company: {
     heading:
-      '<span><span> Merkeziyetsiz</span> <br /><span>Finans</span></span>’ ın geleceğini birlikte inşa etmekte yardımcı olun',
-    hero_title: 'Herkes için gerçekten özgür ve merkeziyetsiz bir finansal ağ',
+      '<br /><span><span> Merkeziyetsiz</span> <br /><span>Finans</span></span>’ ın geleceğini inşa ederken bize yardım edin',
+    hero_title:
+      'Herkes için gerçekten özgür ve merkeziyetsiz bir finansal ağ',
     hero_subtitle:
       'Misyonumuz, merkeziyetsizleşme ile birlikte daha özgür ve daha kapsamlı bir finansal sistem oluşturmaktır.',
     hero_subtitle2:
@@ -84,7 +85,7 @@ export default {
     explore: 'Keşfet',
     exchange: 'Borsa',
     coming_soon: 'Yakında',
-    careers: 'Careers',
+    careers: 'Kariyer',
     we_are_hiring: 'İstihdam Sağlıyoruz',
     developers: 'Geliştiriciler',
     documentation: 'Dokümantasyon',
@@ -100,39 +101,39 @@ export default {
   home: {
     waitlist_title:
       '<span>Gerçekten Özgürce </span> ve <br/> <span>Sınırsız </span> <br/> <span>Olanaklarla</span> ticaret yapmaya başlayın.',
-    hero_title: 'Hızlı, Güvenli ve tamamen Merkezisyetsiz Ticaret',
+    hero_title: 'Hızlı, Güvenli ve Tamamen Merkeziyetsiz Ticaret',
     hero_subtitle:
-      'Injective, merkeziyetsiz türev piyasalarının ve sınırsız DeFi’ nin tüm potensiyelini açığa çıkaracak, ilk Layer-2 merkeziyetsiz borsa protokolüdür.',
+      'Injective, merkeziyetsiz türev piyasalarının ve sınırsız DeFi’ nin tüm potansiyelini açığa çıkaracak, ilk Layer-2 merkeziyetsiz borsa protokolüdür.',
     limitless_title:
-      'İlk tamamen <br /> <span>Merkeziyetsiz</span><br /><span>Borsa</span> üzerinde sınırsız ticaret',
+      'İlk kez tamamen <br /> <span>Merkeziyetsiz</span><br /><span>Borsa</span> üzerinde sınırsız ticaret',
     fully_decentralized: 'Tamamen Merkeziyetsiz',
     fully_decentralized_text:
-      'Injective zinciri layer-2 türev piysasa platformu, işlem yürütme koordinatörü(TEC) ve merkeziyetsiz emir defteri olarak hizmet eden tümüyle merkeziyetsiz yan zincir tekrar katmanlayıcı ağıdır. Ana konsensus Tendermint tabanlıdır.',
+      'Injective zinciri Layer-2 türev piyasa platformu, işlem yürütme koordinatörü(TEC) ve merkeziyetsiz emir defteri olarak hizmet eden, tümüyle merkeziyetsiz yan zincir tekrar katmanlayıcı ağıdır. Ana konsensus Tendermint tabanlıdır.',
     layer2speed: 'Layer-2 Hızı',
     layer2speed_text:
-      'Injective zinciri, Ethereum ve ERC-20 tokenleri Injective Zincirine aktarırken iki yönlü Ethereum peg-zone oluşturmanın yanı sıra DeFi uygulamaları için EVM uyumlu olarak bir yürütme ortamı sağlar. Peg-Zone Peggy tabanlı olup, EVM yürütme uygulaması ise Ethermint’ e dayanmaktadır.',
+      'Injective zinciri, Ethereum ve ERC-20 tokenlerini Injective Zincirine aktarırken iki yönlü Ethereum peg-zone oluşturmanın yanı sıra DeFi uygulamaları için EVM uyumlu olarak bir yürütme ortamı sağlar. Peg-Zone Peggy tabanlı olup, EVM yürütme uygulaması ise Ethermint’ e dayanır.',
     orderbook_model: 'Emir Defteri Modeli',
     orderbook_model_text:
-      'Injective emir defteri, tam merkeziyetsiz bir 0x-bazlı emir defteridir. Neredeyse tüm merkeziyetsiz borsalar tarafından kullanılan geleneksel merkezi zincir dışı işlemlerin, yan zincir emirlerinin -zincir üzeri anlaşmaya- dayalı olarak gerçekleşmesiyle sağlanan merkeziyetsiz bir uygulaması, tam merkeziyetsiz bir 0x-bazlı emir defteri ile gerçekleşir.',
+      "Injective emir defteri, tam merkeziyetsiz bir 0x-bazlı emir defteridir. Neredeyse tüm merkeziyetsiz borsalar tarafından kullanılan geleneksel merkezi zincir dışı işlemlerin, yan zincir emirlerinin -zincir üzeri anlaşmaya- dayalı olarak gerçekleşmesiyle sağlanan merkeziyetsiz bir uygulaması, tam merkeziyetsiz bir 0x-bazlı emir defteri ile gerçekleşir.",
     unlimited_markets: 'Limitsiz Piyasalar',
     unlimited_markets_text:
-      'Piyasa oluşturmak tamamen açık ve izinsizdir. Bu da, isteyen herkesin sadece fiyat akışını kullanarak bir piyasa oluşturmasını ve bunu daha büyük olan Injective ekosistemine dağıtmasını mümkün kılar.',
+      'Piyasa oluşturmak tamamen açık ve izinsizdir. Bu da, isteyen herkesin sadece fiyat akışını kullanarak bir piyasa oluşturmasını ve bunu, daha büyük olan Injective ekosistemine dağıtmasını mümkün kılar.',
     zero_gas_fees: 'Sıfır Gas Ücretleri',
     zero_gas_fees_text:
-      'Benzersiz olan Layer-2 yapısı ile, Injective hem ağ yoğunluğunun hem de buna bağlı olarak yüksek ağ ücretlerinin önüne geçebilir. Sıfır gas ücreti yapısı, herhangi birinin yüksek ağ ücretlerini ve ağ gecikmelerini dert etmeden özgürce işlem yapabilmesini sağlar.',
+      'Benzersiz olan Layer-2 yapısı ile Injective, hem ağ yoğunluğunun hem de buna bağlı olarak yüksek ağ ücretlerinin önüne geçebilir. Sıfır gas ücreti yapısı, herhangi birinin yüksek ağ ücretlerini ve ağ gecikmelerini dert etmeden özgürce işlem yapabilmesini sağlar.',
     trailblazing_title:
-      ' <span> <span>Merkeziyetsiz</span> <br /><span>Finans</span></span> ’ın yeni şafağında çığır açan bir yenilik ',
+      ' <br /><span> <span>Merkeziyetsiz</span> <br /><span>Finans</span></span> ’ın yeni şafağında çığır açan bir yenilik ',
     trailblazing_subtitle:
       'Injective Protocol kullanıcılarına, hiç bir kısıtlama olmaksızın, eşi benzeri olmayan yeni merkeziyetsiz türev piyasalarına erişim imkanı tanıyor.',
     trailblazing_item_1: 'Herhangi bir şeyle ticaret yapın',
     trailblazing_item_1_text:
       'Injective size seçtiğiniz herhangi bir türev piyasasını oluşturma ve işlem yapma özgürlüğünü verir.',
-    learn_more: 'Dokümanlarımızı okuyarak daha fazla bilgi edinin',
+    learn_more: 'Dokümanlarımızı keşfedin',
     trailblazing_item_2: 'Getiri(Yield) Kazanın',
     trailblazing_item_2_text:
       'Ethereum, Cosmos ve diğer merkeziyetsiz ağlar üzerinde çapraz-zincir getirisi(yield) kazanın.',
     trailblazing_item_3: 'Topluluğa Ait',
     trailblazing_item_3_text:
-      'Injective Protocol’ ün yönetiminde yer alın ve geleceğini şeillendirin.',
+      'Injective Protocol’ ün yönetiminde yer alın ve geleceğini şekillendirin.',
   },
 }
